eebaf9e50b2056fee2cef4c007d15600f881fae6
Ruslan155 <coperonplay3@gmail.com>
2023-09-18 16:42:35 +0300

    add particles

--- Assets/Scripts/Fruit.cs
+++ Assets/Scripts/Fruit.cs
@@ -7,11 +7,13 @@ public class Fruit : MonoBehaviour
 
     private Rigidbody fruitRigidbody;
     private Collider fruitCollider;
+    private ParticleSystem juiceParticleEffect;
 
     private void Awake()
     {
         fruitRigidbody = GetComponent<Rigidbody>();
         fruitCollider = GetComponent<Collider>();
+        juiceParticleEffect = GetComponentInChildren<ParticleSystem>();
     }
 
     private void Slice(Vector3 direction, Vector3 position, float force)
@@ -20,6 +22,7 @@ public class Fruit : MonoBehaviour
         sliced.SetActive(true);
 
         fruitCollider.enabled = false;
+        juiceParticleEffect.Play();
 
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         sliced.transform.rotation = Quaternion.Euler(0f, 0f, angle);
